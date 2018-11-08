c508f7cd23028d25939238a576fad930a0073c64
Chris <mengtisun@berkeley.edu>
2018-11-08 15:07:14 -0800

    Initial Commit

---
+++ Tripy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tripy
+//
+//  Created by Chris  on 11/8/18.
+//  Copyright © 2018 Chris . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
